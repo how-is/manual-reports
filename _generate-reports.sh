b324d402ef65e0520dc060338338cd0c05047cc4
Ellen Marie Dash <me@duckie.co>
2017-04-03 00:27:14 -0400

    Don't match tempfiles (that is, things with a trailing tilde).

--- _generate-reports.sh
+++ _generate-reports.sh
@@ -4,7 +4,7 @@ set -e
 
 bundle install
 
-for config in $(ls how-is-configs); do
+for config in $(echo how-is-configs/*.yml); do
   bundle exec how_is --config how-is-configs/$config
   # TODO: Don't sleep on last iteration.
   sleep 60 # Sleep 1 minute to avoid ratelimit issues.
