ce59568dfa21e6fdb2df3edd23c62490792c689b
Ellen Marie Dash <me@duckie.co>
2017-04-03 00:22:33 -0400

    try lower ratelimit

--- _generate-reports.sh
+++ _generate-reports.sh
@@ -6,5 +6,5 @@ bundle install
 
 for config in $(ls how-is-configs); do
   bundle exec how_is --config how-is-configs/$config
-  sleep $[5 * 60] # Sleep 5 minutes to avoid ratelimit issues.
+  sleep 60 # Sleep 1 minute to avoid ratelimit issues.
 done
