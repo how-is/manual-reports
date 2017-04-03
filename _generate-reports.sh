023a1208fc0a570bc1c4d0dc72db910e143614ff
Ellen Marie Dash <me@duckie.co>
2017-04-03 00:27:49 -0400

    We don't need to specify the directory anymore.

--- _generate-reports.sh
+++ _generate-reports.sh
@@ -5,7 +5,7 @@ set -e
 bundle install
 
 for config in $(echo how-is-configs/*.yml); do
-  bundle exec how_is --config how-is-configs/$config
+  bundle exec how_is --config $config
   # TODO: Don't sleep on last iteration.
   sleep 60 # Sleep 1 minute to avoid ratelimit issues.
 done
