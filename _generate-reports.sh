f06db3a9409c85c38008026f7b2784a15faf612b
Ellen Marie Dash <me@duckie.co>
2017-04-03 00:29:16 -0400

    make _generate-reports.sh more verbose.

--- _generate-reports.sh
+++ _generate-reports.sh
@@ -5,7 +5,9 @@ set -e
 bundle install
 
 for config in $(echo how-is-configs/*.yml); do
+  echo "Starting: $config"
   bundle exec how_is --config $config
+  echo "Ending:   $config"
   # TODO: Don't sleep on last iteration.
   sleep 60 # Sleep 1 minute to avoid ratelimit issues.
 done
