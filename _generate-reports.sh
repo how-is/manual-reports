6c0f74f4dac5584e6422ef91ccb04103ae3df33c
Ellen Marie Dash <me@duckie.co>
2017-04-03 00:23:03 -0400

    add todo

--- _generate-reports.sh
+++ _generate-reports.sh
@@ -6,5 +6,6 @@ bundle install
 
 for config in $(ls how-is-configs); do
   bundle exec how_is --config how-is-configs/$config
+  # TODO: Don't sleep on last iteration.
   sleep 60 # Sleep 1 minute to avoid ratelimit issues.
 done
